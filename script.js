08b6dcbfa5e225deed74b61d0cfda8e9f55ba881
OmarHegazi94 <edgeofdarkness_x1@hotmail.com>
2020-06-19 19:05:11 +0200

    passing functions as arguments

--- script.js
+++ script.js
@@ -61,40 +61,80 @@
 
 // Primitives vs Objects
 
-var a = 23;
-var b = a;
+// var a = 23;
+// var b = a;
 
-a = 46;
+// a = 46;
 
-console.log(a, b)
+// console.log(a, b)
 
 
-// Objects
-var obj1 = {
-    name: 'John',
-    age: 26
-};
+// // Objects
+// var obj1 = {
+//     name: 'John',
+//     age: 26
+// };
 
-var obj2 = obj1;
+// var obj2 = obj1;
 
-obj1.age = 30;
+// obj1.age = 30;
 
-console.log(obj1)
-console.log(obj2)
+// console.log(obj1)
+// console.log(obj2)
 
-// Functions
-var age = 27;
-var obj = {
-    name: 'Jonas',
-    city: 'Lisbon'
-};
+// // Functions
+// var age = 27;
+// var obj = {
+//     name: 'Jonas',
+//     city: 'Lisbon'
+// };
 
-function change(a, b) {
-    a = 30;
-    b.city = 'San Francisco'
-};
+// function change(a, b) {
+//     a = 30;
+//     b.city = 'San Francisco'
+// };
 
-change(age, obj)
+// change(age, obj)
 
-console.log(age)
-console.log(obj.city)
+// console.log(age)
+// console.log(obj.city)
+
+
+// Passing functions as arguments
+
+
+var years = [1994, 1343, 1334, 2000, 2015];
+
+function arrayClac(arr, fn) {
+    var arrRes = [];
+
+    for(var i = 0; i< arr.length; i++){
+        arrRes.push(fn(arr[i]));
+    }
+
+    return arrRes;
+}
+
+function calculateAge(el) {
+    return 2020 - el;
+}
+
+function isFullAge(el) {
+    return el >= 18;
+}
+
+function maxHeartRate(el) {
+    if(el >= 18 && el <= 21){
+        return Math.round(206.9 - (0.67 * el));
+    } else {
+        return -1
+    }
+}
+
+var ages = arrayClac(years, calculateAge);
+var fullages = arrayClac(ages, isFullAge);
+var rates = arrayClac(ages, maxHeartRate);
+
+console.log(ages);
+console.log(fullages);
+console.log(rates);
