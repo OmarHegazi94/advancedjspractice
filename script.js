031958b73d10b0e003f28e174a20ed5f0fa8129d
OmarHegazi94 <edgeofdarkness_x1@hotmail.com>
2020-06-19 17:19:44 +0200

    basic prototypes

---
+++ script.js
@@ -0,0 +1,32 @@
+//  Function Constructor
+
+var john = {
+    name: 'John',
+    yearOfBirth: 1990,
+    job: 'teacher'
+};
+
+var Person = function(name, yearOfBirth, job) {
+    this.name = name;
+    this.yearOfBirth = yearOfBirth;
+    this.job = job;
+};
+
+Person.prototype.calculateAge = function() {
+    console.log(2020 - this.yearOfBirth)
+};
+
+Person.prototype.lastName = 'Smith';
+
+
+var john = new Person('John', 1990, 'teacher');
+var jane = new Person('Jane', 1890, 'designer');
+var mark = new Person('Mark', 1999, 'Retired');
+
+john.calculateAge();
+jane.calculateAge();
+mark.calculateAge();
+
+console.log(john.__proto__)
+console.log(jane.lastName)
+console.log(mark.lastName)
